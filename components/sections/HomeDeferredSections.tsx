3bd95c151f30d965b5b6e91ae6361d8cc27bfac3
Shourya Singh <shourya124sensei@gmail.com>
2026-04-09 19:55:01 +0530

    temp commit

--- components/sections/HomeDeferredSections.tsx
+++ components/sections/HomeDeferredSections.tsx
@@ -1,18 +1,11 @@
 "use client";
 
-import dynamic from "next/dynamic";
-
-const AboutEvent = dynamic(() => import("@/components/sections/AboutEvent").then((m) => m.AboutEvent));
-
-const EventsSection = dynamic(() => import("@/components/sections/EventsSection").then((m) => m.EventsSection));
-
-const FAQs = dynamic(() => import("@/components/sections/FAQs").then((m) => m.FAQs));
-
-const Prizes = dynamic(() => import("@/components/sections/Prizes").then((m) => m.Prizes));
-
-const ContactUs = dynamic(() => import("@/components/sections/ContactUs").then((m) => m.ContactUs));
-
-const Footer = dynamic(() => import("@/components/sections/Footer").then((m) => m.Footer));
+import { AboutEvent } from "@/components/sections/AboutEvent";
+import { EventsSection } from "@/components/sections/EventsSection";
+import { FAQs } from "@/components/sections/FAQs";
+import { Prizes } from "@/components/sections/Prizes";
+import { ContactUs } from "@/components/sections/ContactUs";
+import { Footer } from "@/components/sections/Footer";
 
 export function HomeDeferredSections() {
   return (
